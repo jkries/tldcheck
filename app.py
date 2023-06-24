48b0268ed5c40a4b7fbd6266ba8bfdc0f0da99c2
Jason Kries <jkries@gmail.com>
2023-06-23 21:55:02 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask,render_template,request
 import whois
+import os
 
 def is_domain_available(domain):
     try:
         get_info = whois.whois(domain)
+        global expirationDate
+        expirationDate = str(get_info.expiration_date)
         return False
     except:
         return True
@@ -16,11 +19,16 @@ def home():
     checkData = "Nothing yet..."
     if request.method == "POST":
         checkDomain = request.form.get("checkDomain")
+        tldType = os.path.splitext(checkDomain) #Check for tdl type
+        tldExt = tldType[1]
+        if tldExt == "":
+            tldExt = ".com"
+            checkDomain = checkDomain + ".com"
         domainOpen = is_domain_available(checkDomain)
         if domainOpen:
             checkData = "<div class='available'>" + checkDomain + " is AVAILABLE! &#9989;</div>"
         else:
-            checkData = "<div class='taken'>" + checkDomain + " is taken. &#10060;</div>"
+            checkData = "<div class='taken' title='"+ expirationDate +"'>" + checkDomain + " is taken. &#10060;</div>"
         return checkData
     return render_template('home.html', checkData = checkData)
  
